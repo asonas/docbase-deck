62efc4ee8f871e9e5e7204735b95949a2391b35b
Yuya Fujiwara <asonas@cookpad.com>
2023-12-26 14:21:36 +0900

    Switch tags

--- src/memo.tsx
+++ src/memo.tsx
@@ -1,8 +1,9 @@
 import { once, emit } from "@tauri-apps/api/event";
 import { atom, useRecoilState } from "recoil";
-import { useEffect } from "preact/hooks";
+import { useEffect, useState } from "preact/hooks";
 import { createTheme, ThemeProvider, Section, SideNav } from 'smarthr-ui'
 import 'smarthr-ui/smarthr-ui.css'
+import Markdown from 'react-markdown'
 
 
 export const memoState = atom({
@@ -24,12 +25,14 @@ export const memosState = atom({
 export function Memo() {
   // const [memo, setMemo] = useRecoilState(memoState);
   const [memos, setMemos] = useRecoilState(memosState);
+  const [selectedTag, setSelectedTag] = useState('nikki');
 
   useEffect(() => {
     if (memos.length === 0) {
       emit("find-memo");
     }
   })
+  const markdown = '# Hi, *Pluto*!'
 
   useEffect(() => {
     const unlisten = once(
@@ -46,26 +49,46 @@ export function Memo() {
     };
   }, [memos]);
 
+  useEffect(() => {
+    console.log("hennkousaretayo");
+  }, [selectedTag]);
+
+  const selectTag = (event) => {
+    setSelectedTag(event.currentTarget.innerText);
+  }
+  const navItems = [
+    {
+      id: 'nikki',
+      title: 'nikki',
+      isSelected: selectedTag === 'nikki',
+    },
+    {
+      id: 'frappe',
+      title: 'frappe',
+      isSelected: selectedTag === 'frappe',
+    },
+  ]
+
   // UI
   const theme = createTheme();
 
   return (
     <ThemeProvider theme={theme}>
-      <div className="flex flex-col md:flex-row ">
-        <div className="w-1/4 md:w-1/5 p-1">
+      <div className="grid grid-cols-1 md:grid-cols-12">
+        <div className="md:col-span-3 p-1">
           <input type="text" className="mt-1 mb-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
           <Section>
-            <SideNav items={[{ id: "nikki", title: "nikki", isSelected: true }, { id: "frappe", title: "frappe"}]} />
+            <SideNav items={navItems} onClick={selectTag} />
           </Section>
         </div>
-        <div className="md:flex-1">
+        <div className="md:col-span-9 p-2">
           {memos && memos.map((memo, index) => (
             <div key={index}>
               <h3>{memo.title}</h3>
+              {memo.id}
               {/* <p>{memo.body}</p> */}
             </div>
           ))}
-
         </div>
       </div>
     </ThemeProvider>
